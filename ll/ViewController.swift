6c213d82b715565c1079e26f71fda03da42593f2
Алина Класс <alinaklass@MacBook-Pro-Alina.local>
2024-12-06 15:16:43 +0300

    first commit

---
+++ ll/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ll
+//
+//  Created by Алина Класс on 06.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
